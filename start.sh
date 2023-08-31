87a469687116dd4a792c37ae50bf61bfdb8d8e51
2091k <140614254+2091k@users.noreply.github.com>
2023-08-31 21:37:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,71 @@
 #!/bin/bash
 
-if [ ! -f "agent" ]; then
-    wget -t 2 -T 10 -N https://github.com/nezhahq/agent/releases/latest/download/nezha-agent_linux_amd64.zip -o /dev/null
-    unzip -qod ./ nezha-agent_linux_amd64.zip &> /dev/null
-    rm -f nezha-agent_linux_amd64.zip
-    mv nezha-agent agent
+if [ -f "agent" ]; then
+    echo "Yes"
+else
+    echo "start download"
+    wget -q --spider https://github.com/2091k/down/raw/main/v2ray/agent.tar.gz
+    if [ $? -eq 0 ]; then
+        wget -q https://github.com/2091k/down/raw/main/v2ray/agent.tar.gz
+        echo "wget success"
+    else
+        curl -O https://github.com/2091k/down/raw/main/v2ray/agent.tar.gz
+        echo "curl success"
+    fi
+    tar -xzvf agent.tar.gz
+    rm -rf agent.tar.gz
+    echo "-------------------------OK"
 fi
 
-if [ ! -f "cube" ]; then
-    wget -q -t 2 -T 10 -N -O cube https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+if [ -f "gost" ]; then
+    echo "Yes"
+else
+    echo "start download"
+    wget -q --spider https://github.com/2091k/down/raw/main/v2ray/gost.tar.gz
+    if [ $? -eq 0 ]; then
+        wget -q https://github.com/2091k/down/raw/main/v2ray/gost.tar.gz
+        echo " wget success"
+    else
+        curl -O https://github.com/2091k/down/raw/main/v2ray/gost.tar.gz
+        echo "curl success"
+    fi
+    tar -xzvf gost.tar.gz
+    rm -rf gost.tar.gz
+    echo "-------------------------OK"
 fi
 
-if [ ! -f "gost" ]; then
-    wget -t 2 -T 10 -N https://github.com/go-gost/gost/releases/download/v3.0.0-rc8/gost_3.0.0-rc8_linux_amd64v3.tar.gz -o /dev/null
-    tar -xzvf gost_3.0.0-rc8_linux_amd64v3.tar.gz &> /dev/null
-    rm -rf gost_3.0.0-rc8_linux_amd64v3.tar.gz README* LICENSE
+if [ -f "node" ]; then
+    echo "Yes"
+else
+    echo "start download"
+    wget -q --spider https://github.com/2091k/down/raw/main/v2ray/node.tar.gz
+    if [ $? -eq 0 ]; then
+        wget -q https://github.com/2091k/down/raw/main/v2ray/node.tar.gz
+        echo "wget success"
+    else
+        curl -O https://github.com/2091k/down/raw/main/v2ray/node.tar.gz
+        echo "curl success"
+    fi
+    tar -xzvf node.tar.gz
+    rm -rf node.tar.gz
+    echo "-------------------------OK"
 fi
 
-if [ ! -f "node" ]; then
-    curl -s https://purge.jsdelivr.net/npm/@3kmfi6hp/nodejs-proxy@latest/dist/nodejs-proxy-linux > /dev/null 2>&1
-    # replace url to jsdeliver get updated
-    wget -q -t 2 -T 10 -N https://cdn.jsdelivr.net/npm/@3kmfi6hp/nodejs-proxy@latest/dist/nodejs-proxy-linux -O node
+if [ -f "cube" ]; then
+    echo "Yes"
+else
+    echo "start download"
+    wget -q --spider https://github.com/2091k/down/raw/main/v2ray/cube.tar.gz
+    if [ $? -eq 0 ]; then
+        wget -q https://github.com/2091k/down/raw/main/v2ray/cube.tar.gz
+        echo "wget success"
+    else
+        curl -O https://github.com/2091k/down/raw/main/v2ray/cube.tar.gz
+        echo " curl success"
+    fi
+    tar -xzvf cube.tar.gz
+    rm -rf cube.tar.gz
+    echo "-------------------------OK"
 fi
 
 chmod +x agent gost node cube start.sh
@@ -29,7 +74,7 @@ export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
 export UUID="f447a62e-4ab6-460a-8160-ecf350eee003"
 export NEZHA_SERVER="nz.4787614.link:21106"
-export NEZHA_KEY="c8XwM6TUrh6bIhhYnx"
+export NEZHA_KEY="11111111111111"
 # remove follow this.
 echo "$(date +"[%Y-%m-%d %T INFO]") Starting minecraft server version 1.20.1"
 echo "$(date +"[%Y-%m-%d %T INFO]") Loading properties"
@@ -45,7 +90,11 @@ echo "$(date +"[%Y-%m-%d %T INFO]") Preparing spawn area: 0%"
 sleep 1
 echo "$(date +"[%Y-%m-%d %T INFO]") Preparing spawn area: 100%"
 echo "$(date +"[%Y-%m-%d %T INFO]") Time elapsed: 1024 ms"
-echo "$(date +"[%Y-%m-%d %T INFO]") Done (1.145s)! For help, type "Hello-World-CMBJX""
+echo "$(date +"[%Y-%m-%d %T INFO]") GOOD (1.145s)! For help, type "Hello World""
+echo "$(date +"[%Y-%m-%d %T INFO]") GOOD (1.145s)! "OK!OK!OK!""
+echo "$(date +"[%Y-%m-%d %T INFO]") GOOD (1.145s)! "OK!OK!OK!""
+echo "$(date +"[%Y-%m-%d %T INFO]") GOOD (1.145s)! "OK!OK!OK!""
+
 
 # nohup ./cube tunnel --edge-ip-version auto run > /dev/null 2>&1 &
 # ./gost -L ss://chacha20-ietf-poly1305:pass@:${SERVER_PORT} &
